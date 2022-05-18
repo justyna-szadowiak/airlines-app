f24d119db0ca609684a95c52c4855a61115cac4a
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-05-18 12:53:42 +0200

    The components has been prepared

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'footer', component: FooterComponent, children: [
+    {path: 'baggage', redirectTo: 'support/baggage', },
+    {path: 'FAQ', redirectTo: 'support/faq'},
+    {path: 'rules&regulations', redirectTo: 'support/rules-and-regulation'},
+    ]},
+  {
+    path: '**',
+    redirectTo: '/404',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    NotFoundComponent,
+    LogInComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
