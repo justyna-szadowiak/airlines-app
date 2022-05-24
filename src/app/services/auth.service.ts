d44ebac9daaddf41177283fff50cb2fb59ff270b
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-05-24 14:09:46 +0200

    Working on service and backend has been started

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,114 @@
+import { Injectable, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../interfaces';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreDocument} from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  userData: any;
+  constructor(
+    private afs: AngularFirestore,
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private ngZone: NgZone
+  ) {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!)
+      }
+    });
+  }
+
+  SignIn(email: string, password: string) {
+    return this.afAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['book-flight']);
+        });
+        this.SetUserData(result.user);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  Register(email: string, password: string) {
+    return this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.SendVerificationMail();
+        this.SetUserData(result.user);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  SendVerificationMail(){
+    return this.afAuth.currentUser
+      .then((u: any) => u.sendVerification())
+      .then(() => {
+        this.router.navigate(['verify-email-address']);
+      });
+  }
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null && user.emailVerified !== false ? true : false;
+  }
+
+  GoogleAuth(){
+    return this.AuthLogin(new auth.GoogleAuthProvider())
+      .then((res: any) => {
+        if(res) {
+          this.router.navigate(['book-flight']);
+        }
+      });
+  }
+
+  AuthLogin(provider: any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['book-flight']);
+        });
+        this.SetUserData(result.user);
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  }
+
+  SetUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `user/${user.id}`
+    );
+    const userData: User = {
+      id: user.id,
+      name: user.name,
+      surname: user.surname,
+      email: user.email,
+      password: user.password,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
+  }
+
+  SignOut() {
+    return this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['']);
+    });
+  }
+}
