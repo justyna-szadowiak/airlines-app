ef62542e6c2629a21d2544e69b85fae9ffc903c3
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-06-16 00:46:50 +0200

    Some major changes has been done on tsconfig and environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,25 @@
-export const environment = {
+export interface Enviornment {
+  production: boolean;
+  apiURL: string;
+  cookieKey: string;
+  firebase: Firebase;
+}
+
+export interface Firebase {
+  projectId: string;
+  appId: string;
+  storageBucket: string;
+  apiKey: string;
+  authDomain: string;
+  messagingSenderId: string;
+  measurementId: string;
+}
+
+export const environment: Enviornment = {
   production: true,
-  firebase: {
+  apiURL: '',
+  cookieKey: 'th',
+  firebase:  {
     projectId: 'airlines-angular',
     appId: '1:598138869343:web:49b5ce194403ebdc33edea',
     storageBucket: 'airlines-angular.appspot.com',
