6d9b021ba69ac3ddbf0e7f15bcb0be705aef0753
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-06-16 00:52:47 +0200

    Start working on logic for searching flights

--- src/app/book-flight/book-flight.component.ts
+++ src/app/book-flight/book-flight.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { start } from 'repl';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { Flight } from '../interfaces';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-book-flight',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-flight.component.scss']
 })
 export class BookFlightComponent implements OnInit {
+  flightControl = new FormControl('');
+  chooseStartingCity: Flight[] = [];
+  flights$: Observable<Flight[]> | undefined | any;
+  flightsItem: Flight[] | null | undefined = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.flights$ = this.apiService.getAllFligths().pipe(
+      catchError(error => {
+        console.error(error);
+        return of(null)
+      })
+    );
+    this.flights$ = this.flightControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
   }
 
+  private _filter(value: Flight[]): Flight[] | void {
+    const statringCity = this.chooseStartingCity
+      .filter(start => start.placeOfDeparture);
+
+      if(statringCity) {
+        return statringCity
+      }
+  }
+
+  
 }
