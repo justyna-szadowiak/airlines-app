0a1765d2b70d7969a573f86c1fa352412c6041e4
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-06-16 13:17:16 +0200

    Footer component has been created, programmed and styled

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  step = 0;
 
-  constructor() { }
+  setStep(index: number) {
+    this.step = index;
+  }
 
-  ngOnInit(): void {
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
   }
 
 }
