007d40c227403d76cdc6369d95a17fd916ddcc60
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-06-27 19:28:04 +0200

    LogIn component in progress

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface ToSignIn {
+  provideEmail: string,
+  providePassword: string
+}
 
 @Component({
   selector: 'app-log-in',
@@ -7,10 +13,35 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
-  constructor() { }
-  //TO DO
-  // constructor(private authService: AuthService) { }
+  public logInForm!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService) {
+  }
+
+  public ngOnInit(): void {
+    this.buildForm();
+  }
+
+  // public logIn(formData: any): void {
+  //   this.authService.SignIn(provideEmail, providePassword)
+  //     .then(
+  //       (error) => console.error(error)
+  //     )
+  // }
+
+  private buildForm(): void {
+    this.logInForm = this.formBuilder.group({
+      'email': ['', [
+        Validators.required
+      ]
+      ],
+      'password': ['', [
+        Validators.required
+      ]
+      ],
+    });
   }
 }
+
+
